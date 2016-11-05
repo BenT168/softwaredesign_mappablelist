fa6878024ff9cf989e173aaa1b4f6637e12f0a29
Ben Tan <bst15@graphic02.doc.ic.ac.uk>
2016-11-05 22:19:09 +0000

    code repolished

--- src/test/java/ic/doc/GeneralListTest.java
+++ src/test/java/ic/doc/GeneralListTest.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.assertTrue;
 
 public class GeneralListTest {
 
-    final GeneralList<Integer> newList = new GeneralList<Integer>(new ArrayList<Integer>());
+    final GeneralList<Integer> newList =
+            new GeneralList<Integer>(new ArrayList<Integer>());
 
     @Test
     public void isListInitialisedAsEmpty() {
         assertTrue(newList.isEmpty());
     }
-
-
+    
 }
